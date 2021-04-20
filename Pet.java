ded0799ba1651e58d6eafcd9b8b7609171077684
Aditya Bilawar <61608353+adityabilawar@users.noreply.github.com>
2021-04-20 17:15:47 -0400

    Add files via upload

---
+++ Pet.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+
+public class Pet {
+	private String name, type;
+	private int age;
+	
+	public Pet() {
+		
+		
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+    public void setType(String type) {
+    	this.type = type;
+	}
+    public void setAge(int age) {
+    	this.age = age;
+    }
+    public String getName() {
+    	return name;
+    } 
+    public String getType() {
+    	return type;
+    }
+    public int getAge() {
+    	return age;
+    }
+    
+
+	public static void main(String[] args) {
+		Scanner key = new Scanner(System.in);
+		
+		
+
+	}
+
+}
